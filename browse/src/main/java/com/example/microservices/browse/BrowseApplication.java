b1afcd7c18f7bdb9d2e5bc0ba68c95126049466e
Mohamed Rashad <mohamed.rashed@brightskiesinc.com>
2026-03-11 00:11:22 +0200

    first

---
+++ browse/src/main/java/com/example/microservices/browse/BrowseApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices.browse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrowseApplication.class, args);
+    }
+
+}
---
+++ supporting/src/main/java/com/example/microservices/supporting/SupportingApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices.supporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportingApplication.class, args);
+    }
+
+}
